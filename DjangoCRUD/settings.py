fd964230fce57068272e2e599f15329a179e4490
Webdev Khawajam <ka479690@gmail.com>
2025-10-04 20:08:27 +0500

    Deploy Render

--- DjangoCRUD/settings.py
+++ DjangoCRUD/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-0)^*qpbyr(*5cuegu(w2mz%wq%8d$3owt4!_jg047r9&q=9tqh
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['employee.onrender.com']
 
 
 # Application definition
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     'employee',
+    'register',
     'crispy_forms',
     'crispy_bootstrap5',
 ]
@@ -118,6 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
